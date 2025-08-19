8b39478efe08d80638ff6fd799bba11d831a263c
Tyler Tweeten <tweeten.tyler@gmail.com>
2025-08-19 15:59:38 -0500

    Fix nodemailer typo: createTransporter -> createTransport

--- personal-page/netlify/functions/contact.js
+++ personal-page/netlify/functions/contact.js
@@ -53,7 +53,7 @@ exports.handler = async (event, context) => {
     });
 
     // Create email transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
       port: process.env.EMAIL_PORT || 587,
       secure: false, // true for 465, false for other ports
